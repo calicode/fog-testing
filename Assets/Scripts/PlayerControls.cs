4a0567b3a22a73506ca83d910218bdabed3361b0
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-02-02 19:44:27 -0600

    initial

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+	public GameObject shot;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+
+		float hMovement = Input.GetAxisRaw ("Horizontal");
+		float firing = Input.GetAxisRaw ("Fire1");
+
+		print ("firing is" + firing);
+
+		transform.Translate (hMovement / 3, 0f, 0f);
+		if (firing != 0) {
+			FireShot ();
+		}
+
+
+
+
+	}
+
+	/// <summary>
+	/// Fires the shot.
+	/// </summary>
+	void FireShot ()
+	{
+		Instantiate (shot, this.transform.position, this.transform.rotation);
+	}
+}
